3a77d37b53103cf6245fb5b3e9805dbe495926a9
TomBCB <82096710+TomBCB@users.noreply.github.com>
2022-07-19 12:17:31 -0500

    Update registerPatientPage.page.jsx

--- src/views/pages/mainPages/registerPatientPage/registerPatientPage.page.jsx
+++ src/views/pages/mainPages/registerPatientPage/registerPatientPage.page.jsx
@@ -1,8 +1,20 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./main.css";
-export default function RegisterPatientPage() {
-    return (
-         <div id="containerL" className="container-fluid">
+export default function RegisterPatientPage(props) {
+    
+  const [emailPaciente,  setEmailPaciente] = useState(props.email == null ? "" : props.paciente.email)
+  const [contraseñaPaciente,  setContraseñaPaciente] = useState(props.contraseña == null ? "" : props.paciente.contraseña)  
+  const [nombrePaciente,  setNombrePaciente] = useState(props.nombre == null ? "" : props.paciente.nombre)   
+  
+  const onEmailPacienteChange = (evt) => {  
+      setEmailPaciente(evt.target.value)  }
+      const onContraseñaPacienteChange = (evt) => {  
+        setContraseñaPaciente(evt.target.value) }
+      
+        const onNombrePacienteChange = (evt) => {  
+          setNombrePaciente(evt.target.value) }
+    
+    return <div id="containerL" className="container-fluid">
     <div className="row">
       <div className="col-1 col-sm-3 col-lg-4"></div>
       <div className="col-10 col-sm-6 col-lg-4">
@@ -13,18 +25,13 @@ export default function RegisterPatientPage() {
           <label for="exampleFormControlInput1" className="form-label"
             >Correo Electronico</label>
           <div className="form-floating mb-3">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+            <input type="email" className="form-control" value={emailPaciente} onChange={onEmailPacienteChange} id="floatingInput" placeholder="name@example.com"/>
             <label for="floatingInput">*nombre@ejemplo.com</label>
           </div>
         </div>
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label"
-            >Contraseña</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
+          <label for="exampleInputPassword1" className="form-label">Contraseña</label>
+          <input type="password" className="form-control" value={contraseñaPaciente} onChange={onContraseñaPacienteChange} id="exampleInputPassword1" />
           <div id="passwordHelp" className="form-text">
             *Ingrese una contraseña que le sea facil de recordar
           </div>
@@ -32,7 +39,7 @@ export default function RegisterPatientPage() {
         <div>
           <label for="exampleInputPassword1" className="form-label"
             >Nombre Completo</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" value={contraseñaPaciente} onChange={onNombrePacienteChange} />
           <div id="passwordHelp" className="form-text">
             *Ingrese sus datos reales
           </div>
