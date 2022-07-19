2cd89f851074937103357e0faa248c37884bcba0
TomBCB <82096710+TomBCB@users.noreply.github.com>
2022-07-19 11:55:07 -0500

    Update doctorPublicQuestionPage.page.jsx

--- src/views/pages/doctorPages/doctorPublicQuestionPage/doctorPublicQuestionPage.page.jsx
+++ src/views/pages/doctorPages/doctorPublicQuestionPage/doctorPublicQuestionPage.page.jsx
@@ -1,8 +1,99 @@
 import { useEffect } from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+import "./scriptsPF";
 
 export default function DoctorPublicQuestionPage(props) {
   useEffect(() => {
     props.setCurrentTabIndex(props.data.index);
   }, []);
-  return <div>Pagina doctor de preguntas publicas</div>;
+  return <div className="container">
+        <div className="row">
+               <div className="container-faq">
+                <div className="title-faq">
+                    <h3>Preguntas Frecuentes</h3>
+                </div>
+
+                <div className="item-faq">
+                    <div className="question">
+                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit ? <span>P</span></h3>
+                        <div claclassNamess="more"><i>+</i></div>
+                    </div>
+                    <div className="answer">
+                        <div className="mb-3">
+                            <label for="exampleFormControlTextarea1" className="form-label">Respuesta del Especialista:</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> 
+                            <button type="button" class="btn btn-outline-primary">Responder</button>
+                            <span>R</span></div> 
+                    </div>
+                </div>
+
+                <div className="item-faq">
+                    <div className="question">
+                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit ? <span>P</span></h3>
+                        <div className="more"><i>+</i></div>
+                    </div>
+                    <div className="answer">
+                        <div className="mb-3">
+                            <label for="exampleFormControlTextarea1" className="form-label">Respuesta del Especialista:</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> 
+                            <button type="button" className="btn btn-outline-primary">Responder</button>
+                            <span>R</span></div> 
+                    </div>
+                </div>
+
+                <div className="item-faq">
+                    <div className="question">
+                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit ? <span>P</span></h3>
+                        <div className="more"><i>+</i></div>
+                    </div>
+                    <div className="answer">
+                        <div className="mb-3">
+                            <label for="exampleFormControlTextarea1" className="form-label">Respuesta del Especialista:</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> 
+                            <button type="button" className="btn btn-outline-primary">Responder</button>
+                            <span>R</span></div> 
+                    </div>
+                </div>
+
+                <div className="item-faq">
+                    <div className="question">
+                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit ? <span>P</span></h3>
+                        <div className="more"><i>+</i></div>
+                    </div>
+                    <div className="answer">
+                        <div className="mb-3">
+                            <label for="exampleFormControlTextarea1" className="form-label">Respuesta del Especialista:</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> 
+                            <button type="button" className="btn btn-outline-primary">Responder</button>
+                            <span>R</span></div> 
+                    </div>
+                </div>
+
+                <div className="item-faq">
+                    <div className="question">
+                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit ? <span>P</span></h3>
+                        <div className="more"><i>+</i></div>
+                    </div>
+                    <div className="answer">
+                        <div className="mb-3">
+                            <label for="exampleFormControlTextarea1" className="form-label">Respuesta del Especialista:</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> 
+                            <button type="button" className="btn btn-outline-primary">Responder</button>
+                            <span>R</span></div> 
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <section className="paginacion">    
+            <ul>
+                <li> <a href="./doctor_preguntas_publicas1.page.jsx" className="active">1</a></li>
+                <li> <a href="./doctor_preguntas_publicas2.page.jsx">2</a></li>
+                <li> <a href="./doctor_preguntas_publicas3.page.jsx">3</a></li>
+            </ul>
+        </section>
+    </div>
 }
